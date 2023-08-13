c95de97a88d364f8dc72e58c06b21b3f6c251bd6
livemehere <rhdxoals@gmail.com>
2023-08-13 17:48:01 +0900

    4.ts-tsc 환경 구성 완

---
+++ ts-tsc(module-importmap)/src/lib/math.ts
@@ -0,0 +1,8 @@
+// import dayjs from "dayjs";
+
+export const  sum = (a: number, b: number) => {
+    return a + b;
+}
+// export const today = ()=>{
+//     return dayjs().format('YYYY-MM-DD HH:mm:ss');
+// }
