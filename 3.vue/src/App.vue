220c6011a593f93e608b6ddeb3df0a8126ffacc6
livemehere <rhdxoals@gmail.com>
2023-08-13 16:07:33 +0900

    3.vue 환경 구성 완

--- 3.vue/src/App.vue
+++ 3.vue/src/App.vue
@@ -3,11 +3,13 @@
     <h1>Vue.js</h1>
     <p>{{ message }}</p>
     <input type="text" v-model="message" placeholder="message"/>
+    <TextView/>
   </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
+import TextView from "./components/TextView.vue";
 const message = ref('hello');
 </script>
 
---
+++ 3.vue/src/components/TextView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eaque iusto necessitatibus nihil repellendus sint tenetur, ullam? Assumenda nemo nihil optio rem repellat repellendus sapiente. Animi ea excepturi hic, ipsum iusto neque nisi odit qui reiciendis sunt. Doloremque eaque explicabo laudantium nobis sed? Adipisci, aliquid animi at cumque hic mollitia optio quasi quibusdam quidem ullam? Accusamus accusantium adipisci corporis quia ratione repellendus tempora. Alias aspernatur at atque cum cupiditate dicta dolorem dolorum eius eligendi eos esse ex, facilis harum id illo illum incidunt iusto laborum minima nam nihil nisi nobis odit omnis optio praesentium quasi quia quibusdam quidem rem repellat repellendus saepe sequi tempore tenetur vero voluptatem. Ad consectetur dolor earum eius error, expedita hic id impedit inventore iure iusto minus nisi, non odio omnis quod tenetur. Accusamus aliquid, animi dolore labore laudantium minus natus nemo odio similique tempora. Ab, autem est incidunt laudantium numquam odio perferendis quidem soluta! Adipisci assumenda autem consectetur deserunt distinctio esse est facilis molestias non odit. Cupiditate eaque molestias quis! Ad commodi eius exercitationem explicabo magnam maiores natus obcaecati optio recusandae rem. Amet eligendi enim, est facilis inventore magni necessitatibus nihil nostrum nulla, odit, quidem quis quo quod repudiandae saepe? Consequatur ea illo ipsum magnam natus perferendis quam! Commodi dolorem ducimus eos esse eum facere, labore maxime neque nesciunt nobis officiis quam quo ratione recusandae veritatis. Expedita laudantium magni quibusdam. Ad deserunt dicta eos est facere maiores minima molestias mollitia necessitatibus, nobis officia repellendus sapiente sunt tempora vitae. Dolorem inventore necessitatibus nihil, nisi officiis quo ratione rem vero! Asperiores, aut culpa deleniti eaque harum, laborum minus odio optio qui ratione rem repellendus saepe tenetur totam, vitae! Ea enim ipsum labore minus molestias, nobis officiis? Ab dolores ducimus harum impedit magni maxime, nihil nisi omnis perspiciatis praesentium reprehenderit, sed tempora voluptatem. Laborum mollitia praesentium provident repellat.
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  div {
+    margin-top: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 10px;
+  }
+</style>
