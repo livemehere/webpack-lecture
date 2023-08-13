e9a06b83e1685733ba1b2b1f6a46df6358dd2d80
livemehere <rhdxoals@gmail.com>
2023-08-13 15:30:08 +0900

    3.vue 환경 구성 완

---
+++ 3.vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Vue.js</h1>
+    <p>{{ message }}</p>
+    <input type="text" v-model="message" placeholder="message"/>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const message = ref('hello');
+</script>
+
+<style scoped>
+ h1 {
+   color: red;
+ }
+</style>
+
---
+++ 3.vue/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {VueLoaderPlugin} = require("vue-loader");
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    devServer: {
+        port: 3000,
+        open: false,
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+    },
+    devtool: isProduction ? false : 'inline-source-map',
+    resolve: {
+        extensions: ['.js', '.vue'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: "public", to: ".", noErrorOnMissing: true},
+            ]
+        }),
+        new MiniCssExtractPlugin(),
+        new VueLoaderPlugin()
+    ],
+}
